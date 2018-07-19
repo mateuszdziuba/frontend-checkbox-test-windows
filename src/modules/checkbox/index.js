5bc084ae136caac72e9ff4150311ced9eaa995de
mateuszdziuba <mkdziuba@gmail.com>
2018-07-19 11:48:02 +0200

    odpowiedz na zadanie rekrutacyjne

--- src/modules/checkbox/index.js
+++ src/modules/checkbox/index.js
@@ -2,15 +2,37 @@
 import React from 'react';
 
 class Checkbox extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isChecked: false
+        }
+    }
+
+    handleInputChange = () => {
+        this.setState(prevState => ({
+            isChecked: !prevState.isChecked
+        }))
+    }
     render() {
+        const { isChecked } = this.state
         return(
             <div className="checkbox">
                 <div className="title">
                     Checkbox
                 </div>
                 <div className="content">
-                    <input type="checkbox" />
+                    <input 
+                        type="checkbox"
+                        checked = {this.state.isChecked}
+                        onChange={this.handleInputChange}
+                    />
                 </div>
+                {!isChecked ? '' :
+                <div className="hidden">
+                    hidden content
+                </div>
+            }
             </div>
         );
     }
